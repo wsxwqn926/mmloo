b9932926db634e38554b0ada2f7346da544969e6
wsxwqn926 <1396630564@qq.com>
2016-09-30 17:03:17 +0800

    修改banner

--- js/index.js
+++ js/index.js
@@ -3,24 +3,55 @@
         init: function(){
             //初始化属性
             this.navWrapper = $('.cg-content');
-            this.navItem = $('.nav-item')
+            this.navItem = $('.nav-item');
+            this.cgTitle =$('.cg-title');
+            this.categorys =$('.categorys')
             this.dropWrapper = $('.cg-con-drop');
             this.dropItem = $('.drop-item');
+
             this.pre = 0;
 
             //添加事件
             this.navWrapperHover();
             this.navHover();
+            this.navnavItem();
+
+        },
+
+        navnavItem :function () {
+            var that = this;
+            this.cgTitle.mouseenter(function () {
+                console.log(that.navWrapper.eq($(this).index()))
+                that.navWrapper.css({
+                    display : "block"
+                })
+            })
+            this.navWrapper.mouseleave(function () {
+                that.navWrapper.css({
+                    display : "none"
+                })
+            })
         },
         navWrapperHover: function(){
+
             var that = this;
             //鼠标移到cg-con-nav的盒子里面，三级菜单显示
-            this.navWrapper.onmouseenter = function(){
+            this.navWrapper.hover(function () {
+                that.dropWrapper.css({
+                    display : "block"
+                })
+            },function () {
+                that.dropWrapper.css({
+                    display : "none"
+                })
+            })
+           /* this.navWrapper.onmouseenter = function(){
+
                 that.dropWrapper.style.display = 'block';
             }
             this.navWrapper.onmouseleave = function(){
                 that.dropWrapper.style.display = 'none';
-            }
+            }*/
         },
         navHover: function(){
             var that = this;
